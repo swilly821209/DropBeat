50f03bffb23f882e357c333fb4e9c01c0c827126
swilly821209 <swilly821209@icloud.com>
2021-07-05 15:01:03 +0800

    加上button 樣式還沒確定

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// 全域 components
+import BaseButton from './components/ui/BaseButton.vue'
+
+const app = createApp(App)
+
+app.component('base-button', BaseButton)
+
+app.use(store).use(router).mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>home</div>
+  <base-button>試試看</base-button>
 </template>
 
 <script>
