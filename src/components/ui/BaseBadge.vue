b6342a1f9b3124736ae92ef625b5d72ccc861ca7
swilly821209 <swilly821209@icloud.com>
2021-07-19 01:37:59 +0800

    activityitem

--- src/components/ui/BaseBadge.vue
+++ src/components/ui/BaseBadge.vue
@@ -1,17 +1,12 @@
 <template>
     <div class=" w-56 h-9 relative">
       <img :src="image" class="rounded-full  border-gray-dark w-9 h-9 absolute z-10">
-      <p class=" flex justify-center items-center rounded-3xl border-gray-light text-sm w-56 h-9 border absolute pl-5 text-gray-light hover:text-white hover:border-white hover:bg-gray-light transition-all">大象體操 Elephant Gym</p>
+      <p class=" flex justify-center items-center rounded-3xl border-gray-light text-sm w-56 h-9 border absolute pl-5 text-gray-light hover:text-white hover:border-white hover:bg-gray-light transition-all">{{ singer }}</p>
     </div>
 </template>
 
 <script>
 export default {
-//   props: ['image'],
-  data () {
-    return {
-      image: 'https://akstatic.streetvoice.com/profile_images/ju/ne/junepan/U54Pf3WxjfvgWArGdTnKav.jpg?x-oss-process=image/resize,m_fill,h_600,w_600,limit_0/interlace,1/quality,q_95/sharpen,80/format,jpg'
-    }
-  }
+  props: ['image', 'singer']
 }
 </script>
--- src/pages/Active.vue
+++ src/pages/Active.vue
@@ -1,5 +1,16 @@
 <template>
-  <activity-item></activity-item>
+  <activity-item
+    v-for="item in activeDatas"
+    :key="item"
+    :img="item.imgSrc"
+    :title="item.title"
+    :time="item.time"
+    :city="item.city"
+    :location="item.location"
+    :singerImg="item.singerImg"
+    :singer="item.singer"
+    >
+  </activity-item>
 </template>
 
 <script>
@@ -7,6 +18,21 @@ import ActivityItem from '../components/ActivityItem.vue'
 export default {
   components: {
     ActivityItem
+  },
+  data () {
+    return {
+      activeDatas: [
+        {
+          imgSrc: require('../assets/images/active/ac001.jpg'),
+          title: '《 穿過夜晚 Go Through the Night 》2021 新歌巡迴',
+          time: '20:20',
+          city: '台北市',
+          location: '海邊的卡夫卡 Kafka by the Sea',
+          singerImg: 'https://akstatic.streetvoice.com/profile_images/ju/ne/junepan/U54Pf3WxjfvgWArGdTnKav.jpg?x-oss-process=image/resize,m_fill,h_600,w_600,limit_0/interlace,1/quality,q_95/sharpen,80/format,jpg',
+          singer: '大象體操 Elephant Gym'
+        }
+      ]
+    }
   }
 }
 </script>
