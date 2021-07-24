8fd363498d31ab01686c94a809e5d72902bdc5e0
formula5041 <formula5041@gmail.com>
2021-07-25 00:03:25 +0800

    '刻版募資管理頁面'

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import BaseCard from './components/ui/BaseCard.vue'
 import BaseTitle from './components/ui/BaseTitle.vue'
 import BaseDate from './components/ui/BaseDate.vue'
 import BaseDialog from './components/ui/BaseDialog.vue'
-import BaseArrow from './components/ui/BaseArrow.vue'
 import ShareSocialMedia from './components/ShareSocialMedia.vue'
 
 import './index.css'
@@ -23,7 +22,6 @@ app.component('base-card', BaseCard)
 app.component('base-title', BaseTitle)
 app.component('base-date', BaseDate)
 app.component('base-dialog', BaseDialog)
-app.component('base-arrow', BaseArrow)
 app.component('share-social-media', ShareSocialMedia)
 
 app.use(store).use(router).mount('#app')
