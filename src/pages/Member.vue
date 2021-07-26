f8f7535eb1d807cad7936b1211e9d8615d3f6cb6
hsiangting1025 <annieliu1025@gmail.com>
2021-07-26 23:33:01 +0800

    更新會員專區更新會員專區CSS

--- src/pages/Member.vue
+++ src/pages/Member.vue
@@ -2,16 +2,22 @@
   <div class="range">
     <base-title title="會員專區" class=" mb-16"></base-title>
     <div>
-        <h3 class="title-sm">正在關注</h3>
-        <find-carousel class="my-8"></find-carousel>
+      <div class="title02 flex">
+        <div class="titleline"></div>
+        <h3 class="text-2xl text-black-backdrop">正在關注</h3>
+      </div>
+        <find-carousel class="artistCarousel"></find-carousel>
     </div>
-    <div>
-      <h3 class="title-sm">想參加活動</h3>
+    <div class="block">
+      <div class="title02 flex">
+        <div class="titleline"></div>
+        <h3 class="text-2xl text-black-backdrop">想參加活動</h3>
+      </div>
       <swiper :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="my-10">
         <svg xmlns="http://www.w3.org/2000/svg"  class="preIcon  preArrow" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(0 40) rotate(-90)" fill="currentColor"/></svg>
         <svg xmlns="http://www.w3.org/2000/svg" class="nextIcon nextArrow"  width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
         <swiper-slide v-for="item in activeDatas" :key="item" class="flex justify-center">
-          <div class=" flex items-center w-9/12 justify-between">
+          <div class="activeRange flex items-center justify-between">
             <activity-item
                 :img="item.imgSrc"
                 :title="item.title"
@@ -27,13 +33,16 @@
         </swiper-slide>
       </swiper>
     </div>
-    <div>
-      <h3 class="title-sm">已參加活動</h3>
+    <div class="block">
+      <div class="title02 flex">
+        <div class="titleline"></div>
+        <h3 class="text-2xl text-black-backdrop">已參加活動</h3>
+      </div>
       <swiper :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="my-10">
         <svg xmlns="http://www.w3.org/2000/svg"  class="preIcon  preArrow" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(0 40) rotate(-90)" fill="currentColor"/></svg>
         <svg xmlns="http://www.w3.org/2000/svg" class="nextIcon nextArrow"  width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
         <swiper-slide v-for="item in activeDatas" :key="item" class="flex justify-center">
-          <div class=" flex items-center w-9/12 justify-between">
+          <div class="activeRange flex items-center justify-between">
             <activity-item
                 :img="item.imgSrc"
                 :title="item.title"
@@ -49,39 +58,47 @@
         </swiper-slide>
       </swiper>
     </div>
-    <div>
-      <h3 class="title-sm">已贊助計畫</h3>
+    <div class="block">
+      <div class="title02 flex">
+        <div class="titleline"></div>
+        <h3 class="text-2xl text-black-backdrop">已贊助計畫</h3>
+      </div>
       <swiper :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="my-10">
         <svg xmlns="http://www.w3.org/2000/svg"  class="preIcon preArrow" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(0 40) rotate(-90)" fill="currentColor"/></svg>
         <svg xmlns="http://www.w3.org/2000/svg" class="nextIcon nextArrow"  width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
         <swiper-slide v-for="item in 10" :key="item" class="flex justify-center z-20">
-          <div class=" flex items-center w-9/12 justify-between">
-            <img class="w-[350px] h-[200px] rounded-3xl " src="https://akstatic.streetvoice.com/features/2021/07/23/e053e36ad38d43f0818b33e7a9d4b108.png?x-oss-process=image/resize,m_fill,h_480,w_1250,limit_0/interlace,1/quality,q_95/sharpen,80/format,jpg">
-            <div class="flex flex-col">
-              <div class="bg-orange text-white leading-none px-1 rounded-3xl inline-block self-end">success!</div>
-              <h4>【 運氣來的若有似無 】專輯募資</h4>
-              <base-badge  class=" mt-2 inline-block" image="https://akstatic.streetvoice.com/profile_images/er/ic/eric198853/Y3w4tbHRLXMLzFxUmW9bb7.jpg?x-oss-process=image/resize,m_fill,h_200,w_200,limit_0/interlace,1/quality,q_95/sharpen,80/format,jpg" singer="KUNG 龔敬文"></base-badge>
+          <div class="activeRange flex items-center justify-between">
+            <div class="fundImgContent">
+              <img class="w-[350px] h-[200px] rounded-3xl cursor-pointer" src="https://akstatic.streetvoice.com/features/2021/07/23/e053e36ad38d43f0818b33e7a9d4b108.png?x-oss-process=image/resize,m_fill,h_480,w_1250,limit_0/interlace,1/quality,q_95/sharpen,80/format,jpg">
+              <div class="fundContent flex flex-col ">
+                <div class="successIcon bg-orange text-white  rounded-3xl  self-end ">success</div>
+                <h4 class="text-lg text-black-backdrop ">運氣來的若有似無 專輯募資</h4>
+                <base-badge  class=" mt-2 inline-block" image="https://akstatic.streetvoice.com/profile_images/er/ic/eric198853/Y3w4tbHRLXMLzFxUmW9bb7.jpg?x-oss-process=image/resize,m_fill,h_200,w_200,limit_0/interlace,1/quality,q_95/sharpen,80/format,jpg" singer="KUNG 龔敬文"></base-badge>
+              </div>
             </div>
-            <div class="flex flex-col justify-evenly h-full border-l border-gray-light pl-5">
-              <p class="text-4xl text-orange">$600</p>
-              <h5 class=" text-black-backdrop text-xl">專輯海報組</h5>
-              <ul class="list-disc text-gray-light text-base">
-                <li>專輯 x1</li>
-                <li>內含：CD、寫真歌詞本、小卡</li>
-                <li>A2 親簽海報</li>
-              </ul>
+            <div class="flex flex-col justify-center h-full border-l border-gray-default pl-5">
+                <p class="text-3xl text-orange ">$600</p>
+                <h5 class="my-1 text-black-backdrop text-base">專輯海報組</h5>
+                <ul class="list-disc text-gray-dark text-sm">
+                  <li>專輯 x1</li>
+                  <li>內含：CD、寫真歌詞本、小卡</li>
+                  <li>A2 親簽海報</li>
+                </ul>
             </div>
           </div>
         </swiper-slide>
       </swiper>
     </div>
     <div>
-       <h3 class="title-sm">我的音樂庫</h3>
-       <div class=" w-80 flex justify-between my-10">
-         <button class=" border-current rounded-2xl border text-gray-light px-2 hover:text-white hover:bg-orange">喜歡歌曲</button>
-         <button class=" border-current rounded-2xl border text-gray-light px-2 hover:text-white hover:bg-orange">播放清單</button>
-         <button class=" border-current rounded-2xl border text-gray-light px-2 hover:text-white hover:bg-orange">最近播放</button>
-       </div>
+      <div class="title02 flex">
+        <div class="titleline"></div>
+        <h3 class="text-2xl text-black-backdrop">我的音樂庫</h3>
+      </div>
+      <div class="selectBtn  flex justify-start space-x-3">
+        <button class="selectBtn01 border-2 border-orange bg-orange rounded-2xl  text-white ">喜歡歌曲</button>
+        <button class="selectBtn01 border-2 border-gray-default rounded-2xl  text-gray-dark  hover:text-orange  hover:border-orange">播放清單</button>
+        <button class="selectBtn01 border-2 border-gray-default rounded-2xl  text-gray-dark  hover:text-orange  hover:border-orange">最近播放</button>
+      </div>
         <music-item
             v-for="item in musicItems"
             @share-social="shareSocial(item.img, item.name, item.singer)"
@@ -202,10 +219,53 @@ export default {
 
 <style scoped>
 .range{
-  padding: 60px 40px 150px 40px;
+  padding: 60px 40px 110px 40px;
+}
+.titleline{
+  background-color: #b5b5b5;
+  width: 5px;
+  height: 30px;
+  margin-right: 10px;
+}
+.artistCarousel{
+  margin: 10px 0 70px 0;
+}
+.block{
+    margin: 0 0 90px 0;
+}
+.activeRange{
+  width: 80%;
+}
+.fundImgContent{
+    /* border:1px solid red; */
+    display: flex;
+    align-items: center;
+}
+.fundContent{
+    display: flex;
+    margin: 0 0 0 20px;
+}
+.successIcon{
+  height: 25px;
+  width: 80px;
+  text-align: center;
+  margin: 0 0 8px 0;
+}
+.selectBtn{
+    margin: 25px 0 30px 0;
+}
+.selectBtn01{
+  width: 85px;
+  height: 30px;
+  padding: 1px 0 0 0;
+}
+@media screen and (max-width: 1380px) {
+  .activeRange{
+    width: 90%;
+  }
 }
 .preIcon {
-   @apply text-gray-light absolute top-1/2 z-10 left-0 cursor-pointer
+  @apply text-gray-light absolute top-1/2 z-10 left-0 cursor-pointer
 }
 .nextIcon{
     @apply text-gray-light absolute top-1/2 z-10 right-0 cursor-pointer
