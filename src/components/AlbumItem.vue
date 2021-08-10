753c836f9d47447f3dd321eb42acd0005744d092
hsiangting1025 <annieliu1025@gmail.com>
2021-08-10 15:55:32 +0800

    更新rwd

--- src/components/AlbumItem.vue
+++ src/components/AlbumItem.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
     <div class="relative">
-      <div v-if="edit" class=" flex mt-4 absolute -top-16 left-0 ">
-        <div class="h5_icon flex flex-col items-center mr-3 cursor-pointer">
+      <div v-if="edit" class=" flex mt-4 absolute sm:-top-16 -top-14 sm:left-0 right-0">
+        <div class="h5_icon flex flex-col items-center sm:mr-3 ml-3 cursor-pointer">
           <span class="editIcon"></span>
           <h5>編輯</h5>
         </div>
-        <div class="h5_icon flex flex-col items-center mr-3 cursor-pointer">
+        <div class="h5_icon flex flex-col items-center sm:mr-3 ml-3 cursor-pointer">
           <span class="deleteIcon"></span>
           <h5>刪除</h5>
         </div>
-        <div class="h5_icon flex flex-col items-center cursor-pointer">
+        <!-- <div class="h5_icon flex flex-col items-center cursor-pointer">
           <span class="openIcon"></span>
           <h5>公開</h5>
-        </div>
+        </div> -->
       </div>
       <img class="singleAlbum" :src="img">
       <div class="mt-2 text-gray-dark">
-      <h3 class="text-base text-black-backdrop">{{ albumName }}</h3>
-      <p v-if="editAlbum" class="text-sm text-gray-dark">{{ year }}・{{ num }}首歌・{{ totalTime }}</p>
-      <p v-if="editMusic" class="text-sm text-gray-dark">{{ year }}・{{ totalTime }}</p>
+      <h3 class="sm:text-base text-sm text-black-backdrop">{{ albumName }}</h3>
+      <p v-if="editAlbum" class="text-xs sm:text-sm text-gray-dark">{{ year }}・{{ num }}首歌・{{ totalTime }}</p>
+      <p v-if="editMusic" class="text-xs sm:text-sm text-gray-dark">{{ year }}・{{ totalTime }}</p>
       </div>
-      <div v-if="editDraft" class=" flex mt-4 absolute top-[-60px] left-0 sm:hidden">
+      <div v-if="editDraft" class=" flex mt-4 absolute sm:top-[-60px] top-[-58px] right-0 sm:hidden">
         <div class="h5_icon flex flex-col items-center mr-3 cursor-pointer">
           <span class="editIcon"></span>
           <h5>編輯</h5>
@@ -31,7 +31,7 @@
           <h5>刪除</h5>
         </div>
       </div>
-      <div v-if="editDraft" class=" hidden mt-4 absolute bottom- right-0 sm:flex">
+      <div v-if="editDraft" class=" hidden mt-4 absolute bottom-0 right-0 sm:flex">
         <div class="h5_icon flex flex-col items-center mr-3 cursor-pointer">
           <span class="editIcon"></span>
           <h5>編輯</h5>
@@ -107,6 +107,14 @@ h5{
   .singleAlbum{
     width: 220px;
     height: 220px;
+  }
+}
+@media screen and (max-width: 640px) {
+  .editIcon, .deleteIcon{
+    margin: 0 0 2px 0;
+  }
+  h5{
+    font-size: 12px;
   }
 }
 
--- src/pages/Musician.vue
+++ src/pages/Musician.vue
@@ -13,7 +13,7 @@
   </base-dialog> -->
   <!-- <div class="h-[500px] bg-50% bg-top bg-gradient-to-t to-gray-light from-black-backdrop bg-no-repeat px-10 pt-[60px]">
     <div class="flex justify-center items-center bg-gray-300 rounded-full w-10 h-10 cursor-pointer"> -->
-  <div class="h-[400px] sm:h-[500px] mb-[100px] sm:mb-[0] bg-50% bg-top bg-gradient-to-t to-gray-light from-black-backdrop bg-no-repeat px-10 pt-[60px]">
+  <div class="rangeTop h-[400px] sm:h-[500px] mb-[100px] sm:mb-[0] bg-50% bg-top bg-gradient-to-t to-gray-light from-black-backdrop bg-no-repeat px-10 pt-[60px]">
     <div>
       <span class="chooseBg"></span>
     </div>
@@ -30,7 +30,7 @@
         </div>
         <div class="approve">
           <div class="approveIcon"></div>
-          <p>已認證藝人</p>
+          <p class="text-sm sm:text-base">已認證藝人</p>
         </div>
       </div>
       <!-- 640以上 -->
@@ -69,31 +69,31 @@
       </div>
     </div>
     <!-- 640以下 -->
-    <div class="block sm:hidden">
-      <div class="flex items-center mt-[-15px] mb-[30px]">
-        <h1 class="text-center text-[28px] sm:text-5xl text-gray-700 sm:text-white font-medium w-full">告五人 Accusefive</h1>
+    <div class=" block sm:hidden">
+      <div class="flex items-center justify-center mt-[-15px] mb-[30px]">
+        <h1 class="text-center text-[28px] mr-2 sm:text-5xl text-gray-700 sm:text-white font-medium ">告五人 Accusefive</h1>
         <span class="editIcon01"></span>
       </div>
       <div>
         <div class="count_contact flex items-start w-full justify-between">
-          <div class="flex w-80 justify-between items-start mt-[20px]">
+          <div class="flex w-full justify-between items-center mt-[20px]">
             <div>
-              <p class="text-[20px] text-black-backdrop">音樂</p>
+              <p class="text-[14px] text-black-backdrop">音樂</p>
               <div class="countNum text-black-backdrop font-bold tracking-wider">16</div>
             </div>
             <div>
-              <p class="text-[18px] text-black-backdrop">粉絲</p>
+              <p class="text-[14px] text-black-backdrop">粉絲</p>
               <div class="countNum text-black-backdrop font-bold tracking-wider">
                 68,250
-                <svg class="pt-2" xmlns="http://www.w3.org/2000/svg" width="25" height="22" viewBox="0 0 20 12.5">
+                <svg class="pt-1" xmlns="http://www.w3.org/2000/svg" width="15" height="20" viewBox="0 0 20 12.5">
                   <path id="_" data-name="%" d="M8.438,1.952a2,2,0,0,1,3.123,0l5.839,7.3A2,2,0,0,1,15.839,12.5H4.161A2,2,0,0,1,2.6,9.251Z" fill="#fe9f8a"/>
                 </svg>
                 <span class="text-base text-orange">3%</span>
               </div>
             </div>
-          </div>
-          <div>
-            <button class="btn text-sm">聯絡管理員</button>
+            <div>
+              <button class="btn text-sm">聯絡管理員</button>
+            </div>
           </div>
         </div>
         <p class="detail text-sm text-gray-dark">
@@ -111,8 +111,8 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="nextArrow block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
       </div>
     </div>
-    <div class="flex flex-col sm:flex-row justify-center sm:justify-start items-center sm:items-start mb-32">
-      <div class="flex flex-col justify-center items-center relative mr-0 sm:mr-12 fullWidth h-[100px] sm:h-full bg-[#B5B5B5]">
+    <div class="flex flex-col sm:flex-row justify-center sm:justify-start items-center w-full sm:items-start mb-20">
+      <div class="pr-2 sm:pr-0 flex flex-col justify-center items-center relative mr-0 sm:mr-6 sm:w-[260px] w-full sm:h-[260px] h-[100px] bg-[#B5B5B5]">
         <select-img
           class="addFile02"
           :file="true"
@@ -121,8 +121,8 @@
         ></select-img>
       </div>
       <!-- 640以上 -->
-      <swiper :slidesPerView="3" :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="allFund h-[370px] mt-[-48px] hidden sm:flex">
-        <swiper-slide v-for="item in myAlbum" :key="item" class="singleFund top-12">
+      <swiper :slidesPerView="3" :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="aa allFund h-[370px] mt-[-48px] hidden sm:flex">
+        <swiper-slide v-for="item in myAlbum" :key="item" class=" singleFund top-12">
           <album-item class="singleAlbum"
             :edit="true"
             :editAlbum="true"
@@ -154,9 +154,15 @@
       </div>
     </div>
     <!-- 專輯草稿 -->
-    <base-title title="專輯草稿"></base-title>
-    <div class="justify-start items-start mb-32 hidden sm:flex">
-      <swiper :slidesPerView="4" :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="allFund h-[370px] mt-[-48px] w-full">
+    <div class="flex justify-between mt-5">
+      <base-title title="專輯草稿"></base-title>
+      <div class="relative hidden sm:flex items-center top-[-8px] z-10 justify-between w-16" >
+        <svg xmlns="http://www.w3.org/2000/svg" class="preArrowA block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(0 40) rotate(-90)" fill="currentColor"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="nextArrowA block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
+      </div>
+    </div>
+    <div class="justify-start items-start mt-5 mb-20 ml-3 hidden sm:flex">
+      <swiper :slidesPerView="4" :navigation="{nextEl: '.nextArrowA', prevEl: '.preArrowA'}" class="allFund h-[370px] mt-[-48px] w-full">
         <swiper-slide v-for="item in draftAblum" :key="item" class="singleAlbum top-12">
           <album-item class="singleAlbum"
             :editDraft="true"
@@ -172,7 +178,7 @@
       </swiper>
     </div>
     <!-- RWD  -->
-    <div class="justify-start flex-col items-center mb-32 flex sm:hidden mt-[50px]">
+    <div class="justify-start flex-col items-center mb-16 flex sm:hidden mt-[50px]">
       <swiper :slidesPerView="2" :navigation="{nextEl: '.nextArrowA', prevEl: '.preArrowA'}" class="allFund h-[250px] mt-[-48px] w-full">
         <swiper-slide v-for="item in draftAblum" :key="item" class="singleFund top-12">
           <album-item class="singleAlbum"
@@ -192,15 +198,15 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="nextArrowA block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
       </div>
     </div>
-    <div class="flex justify-between mt-5">
+    <div class="flex justify-between sm:mt-5 mt-2">
       <h4 class="t3">我的音樂</h4>
       <div class="relative hidden sm:flex items-center top-[-8px] z-10 justify-between w-16" >
-        <svg xmlns="http://www.w3.org/2000/svg" class="preArrow block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(0 40) rotate(-90)" fill="currentColor"/></svg>
-        <svg xmlns="http://www.w3.org/2000/svg" class="nextArrow block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="preArrowB block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(0 40) rotate(-90)" fill="currentColor"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="nextArrowB block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
       </div>
     </div>
-    <div class="flex flex-col sm:flex-row justify-center sm:justify-start items-center sm:items-start mb-32">
-      <div class="flex flex-col justify-center items-center relative mr-0 sm:mr-12 fullWidth h-[100px] sm:h-full bg-[#B5B5B5]">
+    <div class="flex flex-col sm:flex-row justify-center sm:justify-start items-center sm:items-start w-full mb-20">
+      <div class="flex flex-col justify-center items-center relative sm:mr-7  sm:w-[260px] w-full sm:h-[260px] h-[100px] pr-2 sm:pr-0 bg-[#B5B5B5]">
         <select-img
           @click="uploadMusic"
           class=" addFile02"
@@ -210,7 +216,7 @@
         ></select-img>
       </div>
       <!-- 640以上 -->
-      <swiper :slidesPerView="3" :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="allFund h-[370px] mt-[-48px] hidden sm:flex">
+      <swiper :slidesPerView="3" :navigation="{nextEl: '.nextArrowB', prevEl: '.preArrowB'}" class="allFund h-[370px] mt-[-48px] hidden sm:flex">
         <swiper-slide v-for="item in myMusic" :key="item" class="singleFund top-12">
           <album-item class="singleAlbum"
             :edit="true"
@@ -224,7 +230,7 @@
         </swiper-slide>
       </swiper>
       <!-- 640以下 -->
-      <swiper :slidesPerView="2" :navigation="{nextEl: '.nextArrows', prevEl: '.preArrows'}" class="allFund h-[250px] mt-[35px] w-full sm:hidden">
+      <swiper :slidesPerView="2" :navigation="{nextEl: '.nextArrowsB', prevEl: '.preArrowsB'}" class="allFund h-[250px] mt-[35px] w-full sm:hidden">
         <swiper-slide v-for="item in myMusic" :key="item" class="singleFund top-12">
           <album-item class="singleAlbum"
             :edit="true"
@@ -243,9 +249,15 @@
       </div>
     </div>
     <!-- 音樂草稿  -->
-    <base-title title="音樂草稿"></base-title>
-    <div class="justify-start items-start mb-32 hidden sm:flex">
-      <swiper :slidesPerView="4" :navigation="{nextEl: '.nextArrow', prevEl: '.preArrow'}" class="allFund h-[370px] mt-[-48px] w-full">
+    <div class="flex justify-between sm:mt-5 mt-2">
+      <base-title title="音樂草稿"></base-title>
+      <div class="relative hidden sm:flex items-center top-[-8px] z-10 justify-between w-16" >
+          <svg xmlns="http://www.w3.org/2000/svg" class="preArrowM block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(0 40) rotate(-90)" fill="currentColor"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="nextArrowM block text-gray-light cursor-pointer" width="25" height="40" viewBox="0 0 25 40"><path id="next" d="M17.657,2.928a3,3,0,0,1,4.685,0L36.1,20.126A3,3,0,0,1,33.758,25H6.242A3,3,0,0,1,3.9,20.126Z" transform="translate(25) rotate(90)" fill="currentColor"/></svg>
+      </div>
+    </div>
+    <div class="justify-start items-start mt-5 mb-32 hidden sm:flex">
+      <swiper :slidesPerView="4" :navigation="{nextEl: '.nextArrowM', prevEl: '.preArrowM'}" class="allFund h-[370px] mt-[-48px] w-full">
         <swiper-slide v-for="item in draftMusic" :key="item" class="singleFund top-12">
           <album-item class="singleAlbum"
             :editDraft="true"
@@ -351,6 +363,7 @@ export default {
 </script>
 
 <style scoped>
+
 .chooseBg{
   width: 35px;
   height: 35px;
@@ -516,8 +529,9 @@ h4{
     /* margin: 0 -15px 0 0; */
 }
 .allFund{
-  /* border:1px solid red; */
+  /* border:1px solid blue; */
   transform: scale(1.02);
+  /* width:300px; */
 }
 .singleFund{
   padding: 0 12px 0 12px;
@@ -544,6 +558,35 @@ h4{
   }
 }
 @media  screen and (max-width: 640px) {
+  .rangeTop{
+    /* border:1px solid red; */
+    width: 100%;
+    padding:60px 20px 0 20px;
+  }
+  .range{
+    padding:10px 20px 0 20px;
+  }
+  .editIcon01{
+    width: 16px;
+    height: 16px;
+    margin: 0 0 50px 0;
+  }
+  .editIcon02{
+      width: 16px;
+      height: 16px;
+  }
+  .approveIcon{
+      width: 18px;
+      height: 18px;
+      background-image: url("../assets/icon/approve.svg");
+      margin: 0 5px 2px 0;
+  }
+  h4{
+    font-size: 28px;
+  }
+  .t3{
+      margin: 30px 0 10px 0;
+  }
   ::v-deep .singleAlbum{
     width: 150px;
     height: 150px;
@@ -570,15 +613,15 @@ h4{
       margin: 0px 0px 0px 0px;
   }
   .countNum{
-    font-size: 22px;
+    font-size: 26px;
     margin: -5px 0 0 0;
   }
   .btn{
       width: 80px;
       font-size: 12px;
       padding: 2px 5px;
-      margin-top: 35px;
-      margin-left: 10px;
+      /* margin-top: 35px; */
+      /* margin-left: 10px; */
   }
   ::v-deep .circle{
     /* border:1px solid red; */
